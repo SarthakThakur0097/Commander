8b063d41338c03629909de545c7263404e372284
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-07-27 13:12:01 -0400

    Implemented DTO's

---
+++ Commander/Profiles/CommandsProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Commander.Dtos;
+using Commander.Models;
+
+namespace commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
